57f2e876e59c95542871d1d3856f6f2cd1d261d4
UlmM35 <125765792+UlmM35@users.noreply.github.com>
2025-02-28 15:25:04 +0200

    Added button to generate new squares

--- logic.js
+++ logic.js
@@ -1,16 +1,27 @@
-function generateDivs() {
+function generateDivs(size = 64) {
     const container = document.querySelector("#container");
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < size/4; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
         container.appendChild(row);
-        for (let j = 0; j < 16; j++){
+        for (let j = 0; j < size/4; j++){
             const square = document.createElement("div");
             square.classList.add("square");
+            square.addEventListener("mouseover", (event) => {
+                let target = event.target;
+
+                target.setAttribute("style", "background-color: blue");
+            });
             row.appendChild(square);
         }
     }
 }
 
-generateDivs()
+const squareContainer = document.querySelector("#container");
 
+const button = document.querySelector("#amount");
+
+button.addEventListener("click", () => {
+    const size = prompt("Enter the amount of squares you want. Max 100");
+    generateDivs(size);
+});
